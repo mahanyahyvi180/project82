b860fa78bbc2d3527e265e01454669aec46bdfcd
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-10 10:38:09 +0330

    Add files via upload

---
+++ ConsoleApp82/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp82
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+
+                Console.Write("enter number:");
+                int num1 = int.Parse(Console.ReadLine());
+                Console.Write("enter another number:");
+                int num2 = int.Parse(Console.ReadLine());
+                Console.WriteLine(num1 / num2);
+                Console.ReadLine();
+            }
+            catch
+            {
+                Console.WriteLine("error");
+                Console.ReadLine();
+            }
+        }
+    }
+}
